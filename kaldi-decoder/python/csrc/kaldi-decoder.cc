6df9896f14db3c43e17b215995ced38dcbd91fbf
Fangjun Kuang <csukuangfj@gmail.com>
2023-09-30 17:20:15 +0800

    Add ctc decoder

--- kaldi-decoder/python/csrc/kaldi-decoder.cc
+++ kaldi-decoder/python/csrc/kaldi-decoder.cc
@@ -3,11 +3,17 @@
 // Copyright (c)  2022  Xiaomi Corporation
 
 #include "kaldi-decoder/python/csrc/kaldi-decoder.h"
+#include "kaldi-decoder/python/csrc/decodable-ctc.h"
+#include "kaldi-decoder/python/csrc/decodable-itf.h"
+#include "kaldi-decoder/python/csrc/faster-decoder.h"
 
 namespace kaldi_decoder {
 
 PYBIND11_MODULE(_kaldi_decoder, m) {
   m.doc() = "pybind11 binding of kaldi-decoder";
+  PybindDecodableItf(&m);
+  PybindFasterDecoder(&m);
+  PybindDecodableCtc(&m);
 }
 
 } // namespace kaldi_decoder
--- kaldi-decoder/python/csrc/kaldi-decoder.h
+++ kaldi-decoder/python/csrc/kaldi-decoder.h
@@ -5,7 +5,7 @@
 #ifndef KALDI_DECODER_PYTHON_CSRC_KALDI_DECODER_H_
 #define KALDI_DECODER_PYTHON_CSRC_KALDI_DECODER_H_
 
-// #include "pybind11/eigen.h"
+#include "pybind11/eigen.h"
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
