8085869d08d01239e6d88ee930103cc9c99f153f
Fangjun Kuang <csukuangfj@gmail.com>
2023-10-07 11:56:56 +0800

    Support sharing memory with log_probs in DecodableCtc. (#5)

--- kaldi-decoder/csrc/decodable-ctc.cc
+++ kaldi-decoder/csrc/decodable-ctc.cc
@@ -8,7 +8,15 @@
 
 namespace kaldi_decoder {
 
-DecodableCtc::DecodableCtc(const FloatMatrix &feats) : feature_matrix_(feats) {}
+DecodableCtc::DecodableCtc(const FloatMatrix &log_probs)
+    : log_probs_(log_probs) {
+  p_ = &log_probs_(0, 0);
+  num_rows_ = log_probs_.rows();
+  num_cols_ = log_probs_.cols();
+}
+
+DecodableCtc::DecodableCtc(const float *p, int32_t num_rows, int32_t num_cols)
+    : p_(p), num_rows_(num_rows), num_cols_(num_cols) {}
 
 float DecodableCtc::LogLikelihood(int32_t frame, int32_t index) {
   // Note: We need to use index - 1 here since
@@ -16,12 +24,12 @@ float DecodableCtc::LogLikelihood(int32_t frame, int32_t index) {
   // construction
   assert(index >= 1);
 
-  return feature_matrix_(frame, index - 1);
+  return *(p_ + frame * num_cols_ + index - 1);
 }
 
-int32_t DecodableCtc::NumFramesReady() const { return feature_matrix_.rows(); }
+int32_t DecodableCtc::NumFramesReady() const { return num_rows_; }
 
-int32_t DecodableCtc::NumIndices() const { return feature_matrix_.cols(); }
+int32_t DecodableCtc::NumIndices() const { return num_cols_; }
 
 bool DecodableCtc::IsLastFrame(int32_t frame) const {
   assert(frame < NumFramesReady());
